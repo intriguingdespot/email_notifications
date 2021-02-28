2ba2f9c6fe012092ec93f3fea38d862355517d8f
Kesavan <kesavan22.kr@gmail.com>
2021-02-28 11:30:08 +0530

    Testing Schedulder

---
+++ notifications/testing.py
@@ -0,0 +1,31 @@
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = "intriguing.despot@gmail.com"
+EMAIL_PASSWORD = "kesavan@123"
+
+contacts = ["intriguing.despot@gmail.com"]
+
+msg = EmailMessage()
+msg['Subject'] = 'Check out Bronx as a puppy!'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = "intriguing.despot@gmail.com"
+
+msg.set_content('This is a plain text email')
+
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:SlateGray;">This is an HTML Email! from Scdular</h1>
+    </body>
+</html>
+""", subtype='html')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
+    print("Done")
