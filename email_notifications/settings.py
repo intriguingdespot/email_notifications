e20ac96030413a160ce6854c1a40df6edc73702c
KesavanRamalingam <kesavan22.kr@gmail.com>
2021-07-10 07:32:31 +0530

    Test

--- email_notifications/settings.py
+++ email_notifications/settings.py
@@ -118,7 +118,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "**************"
+EMAIL_HOST_USER = "*************"
 EMAIL_HOST_PASSWORD = "***************"
 
 
